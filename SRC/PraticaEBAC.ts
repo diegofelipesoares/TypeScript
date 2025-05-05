d191a45826134eec9a7cf1e2ff2cd4a76d44b336
Diego Soares <diegofelipesoares@gmail.com>
2025-05-05 18:44:48 -0300

    ExercicioEbac

---
+++ SRC/PraticaEBAC.ts
@@ -0,0 +1,9 @@
+function Multiplicar(numero1: number, numero2: number): number {
+    return numero1 * numero2;
+}
+function Saudar(nome1: string): string {
+    return `Olá ${nome1}`;
+}
+
+console.log(Multiplicar(10, 5));
+console.log(Saudar("Diego"));
